eff6aa095877d39d3049b0f1b1b2d1f25d5dfdfa
Marcin Hoppe <marcin.hoppe@gmail.com>
2014-09-04 22:31:05 +0200

    Add app to print results to console

---
+++ Src/MarcinHoppe.FizzBuzz.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MarcinHoppe.FizzBuzz.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 100;
+            var results = Problem.SolveUpTo(N);
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
